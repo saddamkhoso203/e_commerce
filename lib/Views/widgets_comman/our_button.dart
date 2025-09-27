f84dc39918b30313d8e9c0cb36b6b9cf4d70c890
Saddam Khoso <saddamkhoso203@gmail.com>
2025-09-28 02:54:50 +0500

    updated code

--- lib/Views/widgets_comman/our_button.dart
+++ lib/Views/widgets_comman/our_button.dart
@@ -1,11 +1,21 @@
 import '../../consts/consts.dart';
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
-Widget ourButton({onPress, color, textColor, String? title}) {
+Widget ourButton({
+  required VoidCallback? onPress,
+  Color? color,
+  Color? textColor,
+  String? title,
+}) {
   return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-          // ignore: deprecated_member_use
-          primary: color,
-          padding: const EdgeInsets.all(12)),
-      onPressed: onPress,
-      child: title?.text.color(textColor).fontFamily(bold).make());
+    style: ElevatedButton.styleFrom(
+      backgroundColor: color, // updated (instead of primary)
+      padding: const EdgeInsets.all(12),
+    ),
+    onPressed: onPress,
+    child: title != null
+        ? title.text.color(textColor).fontFamily(bold).make()
+        : const SizedBox.shrink(),
+  );
 }
